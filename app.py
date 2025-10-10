08fff869181d47d330894290e98b53aed9c96182
pwngithub <110849398+pwngithub@users.noreply.github.com>
2025-10-10 09:47:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests
 import re
-from streamlit_autorefresh import st_autorefresh  # optional live refresh support
 
 # -------------------------------
 # APP CONFIGURATION
